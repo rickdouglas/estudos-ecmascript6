0d7fdf84e6f34c369970d6ace7cf3c4537d737cc
rickdouglas <rickmiranda23@gmail.com>
2020-02-07 10:29:33 -0300

    Arrow functions

--- bundle.js
+++ bundle.js
@@ -1,19 +1,15 @@
 "use strict";
 
 var vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var newVet = vetor.map(function (item, index) {
-  return item + index;
+var newVet = vetor.map(function (item) {
+  return item * 2;
 });
 console.log(newVet);
-var sum = vetor.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum);
-var filtrar = vetor.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filtrar);
-var find = vetor.find(function (item) {
-  return item === 10;
-});
-console.log(find);
+
+var teste = function teste() {
+  return {
+    nome: 'Rick'
+  };
+};
+
+console.log(teste());
--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 const vetor =[1,2,3,4,5,6,7,8,9,10];
 
-const newVet = vetor.map(function(item, index){
-    return item + index;
-});
-
+const newVet = vetor.map(item => item *2);
 console.log(newVet);
 
-const sum = vetor.reduce(function(total,next){
-    return total + next;
-})
-console.log(sum);
-
-const filtrar = vetor.filter(function(item){
-    return item % 2 === 0;
-});
-console.log(filtrar);
-
-const find = vetor.find(function(item){
-    return item ===10;
-});
-console.log(find);
+const teste = () => (
+    {nome : 'Rick'}
+);
+console.log(teste());
