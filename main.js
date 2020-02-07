c7c46bf358b0b340078a26e66ef65cc5a9a2c6bc
rickdouglas <rickmiranda23@gmail.com>
2020-02-07 15:25:04 -0300

    Template Literals

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
-// REST serve para pegar o resto das propriedades
-
+// Template Literals
 const usuario ={
     nome:'Rick',
     idade:28,
-    empresa :'Somar Contabilidade'
+    status:'Procurando emprego de desenvolvedor junior'
 };
-const{nome, ...resto} = usuario;
+const {nome, idade, status} = usuario; 
+console.log("Eu sou " + nome + " tenho " + idade + " anos e estou " + status);
 
-console.log(nome);
-console.log(resto);
+//Concatenação usando template literals
 
-const array =[1,2,3,4,5];
-
-const [ a,b,...c] = array;
-
-console.log(a,b,c);
-
-function teste (...params){
-    return params;
-}
-console.log(teste(1,2,3,4));
-
-// Spread, serve para propagar os dados
-
-const vetor1 =[1,2,3];
-const vetor2 =[4,5,6];
-
-const vetor3 = [...vetor1, ...vetor2];
-
-console.log(vetor3);
-
-const usuario2 ={
-    nome:'Rick',
-    idade:28
-};
-const usuario3 = {...usuario2, nome:'Rick Douglas'};
-console.log(usuario3);
+console.log(`Meu nome é ${nome} tenho ${idade} e estou ${status}`);
