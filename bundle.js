c63b59746b203c9f31fdf37eeefb3a70e9e664fc
rickdouglas <rickmiranda23@gmail.com>
2020-02-07 14:58:26 -0300

    Atributos REST e SPREAD

--- bundle.js
+++ bundle.js
@@ -1,15 +1,54 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+// REST serve para pegar o resto das propriedades
 var usuario = {
   nome: 'Rick',
-  idade: '28',
-  endereco: {
-    cidade: 'Lavras',
-    estado: 'MG'
-  }
+  idade: 28,
+  empresa: 'Somar Contabilidade'
 };
-console.log(usuario);
+
 var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade;
-console.log(nome, idade, cidade);
+    resto = _objectWithoutProperties(usuario, ["nome"]);
+
+console.log(nome);
+console.log(resto);
+var array = [1, 2, 3, 4, 5];
+var a = array[0],
+    b = array[1],
+    c = array.slice(2);
+console.log(a, b, c);
+
+function teste() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params;
+}
+
+console.log(teste(1, 2, 3, 4)); // Spread, serve para propagar os dados
+
+var vetor1 = [1, 2, 3];
+var vetor2 = [4, 5, 6];
+var vetor3 = [].concat(vetor1, vetor2);
+console.log(vetor3);
+var usuario2 = {
+  nome: 'Rick',
+  idade: 28
+};
+
+var usuario3 = _objectSpread({}, usuario2, {
+  nome: 'Rick Douglas'
+});
+
+console.log(usuario3);
--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const usuario = {
-    nome :'Rick',
-    idade :'28',
-    endereco: {
-        cidade :'Lavras',
-        estado:'MG'
-    },
+// REST serve para pegar o resto das propriedades
+
+const usuario ={
+    nome:'Rick',
+    idade:28,
+    empresa :'Somar Contabilidade'
 };
+const{nome, ...resto} = usuario;
 
-console.log(usuario);
+console.log(nome);
+console.log(resto);
 
-const { nome, idade, endereco:{cidade} } = usuario;
+const array =[1,2,3,4,5];
 
-console.log(nome, idade, cidade);
+const [ a,b,...c] = array;
+
+console.log(a,b,c);
+
+function teste (...params){
+    return params;
+}
+console.log(teste(1,2,3,4));
+
+// Spread, serve para propagar os dados
+
+const vetor1 =[1,2,3];
+const vetor2 =[4,5,6];
+
+const vetor3 = [...vetor1, ...vetor2];
+
+console.log(vetor3);
+
+const usuario2 ={
+    nome:'Rick',
+    idade:28
+};
+const usuario3 = {...usuario2, nome:'Rick Douglas'};
+console.log(usuario3);
