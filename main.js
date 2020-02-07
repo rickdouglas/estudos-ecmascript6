fc75ba754d6954d3ad5a9e6f6158128a743203e4
rickdouglas <rickmiranda23@gmail.com>
2020-02-07 00:44:08 -0300

    Classes com ecmascript 6

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-alert('teste');
+class List{
+    constructor(){
+        this.data = [];
+    }
 
-class Teste {
-    metodo(){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data)
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Rick';
+    }
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+
+
+}
+
+const MinhaLista = new TodoList();
+document.getElementById('novo-todo').onclick = function(){
+    MinhaLista.add('novo-todo');
+}
+MinhaLista.mostraUsuario();
